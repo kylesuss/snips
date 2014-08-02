140e158bd3d0582c1897dae0c230e5c3088f0b0d
Kyle Suss <susskyle@gmail.com>
2014-08-02 17:05:09 -0600

    added scrolltop w/out jquery

---
+++ js/scrollTop.js
@@ -0,0 +1,2 @@
+// ScrollTop without jQuery
+var scrollLevel = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
