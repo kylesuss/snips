9e47648612aaf01010364949c125e59287380e30
Kyle Suss <susskyle@gmail.com>
2014-08-18 14:21:56 -0600

    added jslint comment and a missing closing bracket

--- grunt/gettingStarted.js
+++ grunt/gettingStarted.js
@@ -1,9 +1,15 @@
+/*jslint node: true */
+
+'use strict';
+
 module.exports = function(grunt) {
   grunt.initConfig({
   
-  // Put plugin stuff here
+    // Put plugin stuff here
+
+  });
 
   grunt.loadNpmTasks('insert-grunt-plugin');
 
   grunt.registerTask('default', ['insert-grunt-task']);
-}
+};
